31fc686c525aaae3e94b36df0464b9a660acf234
igel <igel.yulian@mail.ru>
2012-09-12 11:10:49 +0400

    edit shop

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
-	has_and_belongs_to_many :shops
+	#has_and_belongs_to_many :shops
+
+  has_many :shops, through: :shop_products
+  has_many :shop_products
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,9 +2,11 @@ class Shop < ActiveRecord::Base
 	belongs_to :area
 	belongs_to :chain
 
-	has_and_belongs_to_many :products
+	#has_and_belongs_to_many :products
+  has_many :products, through: :shop_products
+  has_many :shop_products
 
 	has_many :users, through: :user_shops
-  	has_many :user_shops
+  has_many :user_shops
 
 end
