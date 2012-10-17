ca867bd3fe636392e271b03498014b1ab543505b
igel84 <igel.yulian@mail.ru>
2012-10-18 00:58:23 +0400

    add_areas_location

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,6 +2,8 @@ class Area < ActiveRecord::Base
 	belongs_to :city
 	has_many :shops
 
+  has_and_belongs_to_many :chains, :uniq => true#, join_table: 'chains' #class_name: 'AreaChain'
+
   def to_s
     name
   end
